54c44d391e866de85e9f21e212469c76f9c2c935
J. Sebastian Valle <vallebarbaranj@gmail.com>
2025-04-09 10:15:53 -0500

    refactor(cart.php): implement includes

--- src/store/views/user/cart.php
+++ src/store/views/user/cart.php
@@ -18,29 +18,15 @@ require_once __DIR__ . '/../../../router.php';
     <?php
     include '../../includes/navbar.php'
     ?>
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-            <div class="col-md-1 sidebar">
-                <div class="cart-summary mb-5 w-100">
-                    <h2>Carrito</h2>
-                    <p>- Productos 165</p>
-                    <p>- Total 150.00.00</p>
-                    <p>- Ver Carrito</p>
-                </div>
-                
-                <div class="user-section">
-                    <h2>Isaac Cardona</h2>
-                    <p>- Gestionar productos</p>
-                    <p>- Gestionar categorías</p>
-                    <p>- Gestionar pedidos</p>
-                    <p>- Mis pedidos</p>
-                    <p>- Cerrar sesión</p>
-                </div>
-            </div> 
+            <?php
+            include '../../includes/aside.php'
+            ?>
             
             <!-- Main Content -->
-            <div class="col-md-9 main-content">
+            <div class="col-md-10 main-content">
                 <h1>Carrito de Compras</h1>
                 
                 <!-- Column Headers -->
@@ -161,12 +147,9 @@ require_once __DIR__ . '/../../../router.php';
         </div>
     </div>
     
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="container">
-            <p class="mb-0">Desarrollado por Grupo n° 2 | SENA CDITI 2025</p>
-        </div>
-    </footer>
+    <?php
+    include '../../includes/footer.php'
+    ?>
     
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
