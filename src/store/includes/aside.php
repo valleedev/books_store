af16e98ce0e25677cfb382714d21fdf80b216c06
juanessoto17 <sotojuanes15>
2025-04-12 12:41:30 -0500

    redirecciones funcionales del sidebar

--- src/store/includes/aside.php
+++ src/store/includes/aside.php
@@ -10,7 +10,7 @@
         <div class="guest-options mt-3">
             <ul class="nav flex-column px-2">
                 <li class="nav-item">
-                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="#">
+                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="<?= USER ?>see_orders.php">
                         Mis pedidos
                         <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
                             <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
@@ -18,7 +18,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="#">
+                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="<?= PRODUCTS ?>products.php">
                         Gestionar pedidos
                         <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
                             <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
@@ -26,7 +26,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="#">
+                    <a class="icon-link icon-link-hover text-dark text-decoration-none" href="<?= PRODUCTS ?>categories.php">
                         Gestionar categorías
                         <svg xmlns="http://www.w3.org/2000/svg" class="bi" viewBox="0 0 16 16" aria-hidden="true">
                             <path d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
